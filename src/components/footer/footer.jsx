237167252896357a3094d555122e4e5e243b3c5e
Dylan <dmsosa2@espe.edu.ec>
2026-06-13 02:33:42 -0500

    Actualizada dirección de la ESPE en el Footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.css';
+
+export const Footer = () => {
+    const anioActual = new Date().getFullYear();
+
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.text}>Todos los derechos reservados - ESPE {anioActual}.</p>
+            <p className={styles.text}>Dirección: Av. del Ejército 18-01 y Av. 12 de Abril, Quito, Ecuador</p>
+        </footer>
+    );
+};
